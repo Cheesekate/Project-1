5bb985322465ca4c442a75da1b981ae77728c682
Haven <stackandslash@gmail.com>
2020-05-19 17:00:04 -0700

    First pass, weathermap call 1 complete

--- script.js
+++ script.js
@@ -12,18 +12,33 @@ $(function() {
   
 //Back-end code playground kicks in here:
 
-var APIKey = "qdZu7Y7hMt3KGPxrdiPLP6B4TNiFoYZC";
-var cityName = "Phoenix";
-var todayDate = "2020-05-20T23:59:59Z"; //this is going to change a bit, thinking midnight day after?
-//var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&city=" + cityName + "&endDateTime=" + todayDate + "&radius=200" + "&apikey=" + APIKey
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&lat=" + exampleLat + "&long=" + exampleLong + "&endDateTime=" + todayDate + "&radius=200" + "&apikey=" + APIKey
-var exampleLat = "33.4582"
-var exampleLong = "-112.0691"
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-});
+$("#searchBtn").on("click", function (response){
+  response.preventDefault();
+  const citypass = $("#searchbox").val();
+  cityPull(citypass);
+  });
+  
+  function cityPull(cityname){
+  
+      var APIKey = "c0708fd314d4abadfb6401261f72c41f";
+      var queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=" + APIKey;
+      var lat = 0;
+      var lon = 0;
+  
+      $.ajax({
+      url: queryURL,
+      method: "GET",
+      }).then(function (response) {
+      const whicon = "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png"; //gets our weather icon
+      const temp = response.main.temp;
+      lat = response.coord.lat;
+      lon = response.coord.lon;
+      console.log(response);
+      console.log(lat, lon);
+      //this pastes the icon tag and temperature readout straight into a text tag by ID. 
+      $("#cityWeather").html("<img src='" + whicon + "' alt=' Projected weather icon'>" + temp.toFixed(0) + "°F");
+      
+  
+  });
+      
+  }
